ac269d427c5c90de4e5ff3f1a8a3d430e33b0816
yasinarafat39 <yasinarafat2557@gmail.com>
2022-09-04 03:53:00 +0600

    responsive and many problem solving with google meet

--- js/news.js
+++ js/news.js
@@ -13,8 +13,6 @@ const getNews = (category_id) => {
 const categoryName = document.getElementById('category-name');
 
     getNews('01');
-    
-
 document.getElementById('category-02').addEventListener('click', function(){
     getNews('02');
     categoryName.innerText = 'Regular news';
@@ -55,38 +53,43 @@ const displayNews = allNews => {
     }
 
     const newsContainer = document.getElementById('news-container');
-    newsContainer.innerHTML = ``;
+    newsContainer.innerHTML = '';
     allNews.forEach(singleNews => {
 
-        const {thumbnail_url, title, total_view, details, _id} = singleNews;
-        const {name, published_date, img} = singleNews.author;
+        // const {thumbnail_url, title, total_view, details, _id} = singleNews;
+        // const {name, published_date, img} = singleNews.author;
 
         const newsDiv = document.createElement('div');
-        newsDiv.classList.add('card', 'mb-3', 'w-full');
+        
         newsDiv.innerHTML = `
-        <div class="row g-0">
-            <div class="col-md-3">
-                <img src="${thumbnail_url}" class="img-fluid w-full  rounded-start" alt="...">
-            </div>
-            <div class="col-md-9">
+        <div class="container mt-5 col-lg-12">
+        <div class="card mb-3 shadow-sm">
+
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${singleNews.image_url}" class=" img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">${title}</h5>
-                    <p class="card-text">${details.length > 100 ? details.slice(0, 400) + "..." : details}</p>
+                    <h5 class="card-title">${singleNews.title}</h5>
+                    <p class="card-text">${singleNews.details.length > 100 ?singleNews.details.slice(0, 400) + "..." : details}</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="d-flex align-items-center">
-                            <img src="${img === 'undifined' ? 'N/A' : img}" class="rounded-circle border" alt="author_name" width="40" height="40">
+                            
                             &nbsp; &nbsp;
                             <div>
-                                <p class="my-0">${name}</p>
-                                <small>${published_date}</small>
+                                
+                                <small>${singleNews.author.published_date}</small>
                             </div>
                         </div>
-                        <p class="mt-0 mb-0"> <i class="fa-duotone fa-eye"></i> ${total_view}</p>
+                        <p class="mt-0 mb-0"> <i class="fa-regular fa-eye"></i> ${singleNews.total_view}</p>
                         <!-- Button trigger modal -->
-                        <i onclick="showDetailsOfNews('${_id}')" style='font-size:24px' class='far' role="button" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">&#xf35a;</i>
+                        <i onclick="showDetailsOfNews('${singleNews._id}')" style='font-size:24px' class='far' role="button" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">&#xf35a;</i>
                     </div>
                 </div>
             </div>
+            </div>
+        </div>
         </div>
         `;
         newsContainer.appendChild(newsDiv);
