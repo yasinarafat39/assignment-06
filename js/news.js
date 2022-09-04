4cff394f8c2a950e45c7e021534c996b044bdd94
yasinarafat39 <yasinarafat2557@gmail.com>
2022-09-04 08:53:22 +0600

    added new categories list without spinner

--- js/news.js
+++ js/news.js
@@ -1,45 +1,43 @@
 
 
+const newsCategoriesContainer = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        .then(res => res.json())
+        .then(data => displayNewsCategories(data.data.news_category))
+}
+
+const displayNewsCategories = (allCategories) => {
+    const newsCategoriesContainer = document.getElementById('news-category-container');
+
+    allCategories.forEach(category => {
+        // console.log(category);
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <a onclick="getNews('${category.category_id}')">${category.category_name}</a>
+        `;
+        newsCategoriesContainer.appendChild(li);
+    });
+}
+
+
+
 const loadingSpinner = document.getElementById('loadingSpinner');
 const getNews = (category_id) => {
     // spinner start
-    loadingSpinner.classList.remove('d-none');
+    // loadingSpinner.classList.remove('d-none');
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    // console.log(url)
     fetch(url)
     .then(res => res.json())
     .then(data => displayNews(data.data))
 }
 
-const categoryName = document.getElementById('category-name');
-
-    getNews('01');
-document.getElementById('category-02').addEventListener('click', function(){
-    getNews('02');
-    categoryName.innerText = 'Regular news';
-});
-document.getElementById('category-03').addEventListener('click', function(){
-    getNews('03');
-    categoryName.innerText = 'International news';
-});
-document.getElementById('category-04').addEventListener('click', function(){
-    getNews('04');
-    categoryName.innerText = 'Sports';
-});
-document.getElementById('category-05').addEventListener('click', function(){
-    getNews('05');
-    categoryName.innerText = 'Entertainment';
-});
-document.getElementById('category-06').addEventListener('click', function(){
-    getNews('06');
-    categoryName.innerText = 'Cultur';
-});
-document.getElementById('category-07').addEventListener('click', function(){
-    getNews('07');
-    categoryName.innerText = 'All news';
-});
 
 const displayNews = allNews => {
-    console.log(allNews);
+    
+    allNews.sort((a,b) => {
+        return b.total_view - a.total_view;
+    });
 
     document.getElementById('total-item').innerText = allNews.length;
     // Error Message
@@ -56,8 +54,8 @@ const displayNews = allNews => {
     newsContainer.innerHTML = '';
     allNews.forEach(singleNews => {
 
-        // const {thumbnail_url, title, total_view, details, _id} = singleNews;
-        // const {name, published_date, img} = singleNews.author;
+        const {image_url, title, total_view, details, _id} = singleNews;
+        const {name, published_date, img} = singleNews.author;
 
         const newsDiv = document.createElement('div');
         
@@ -67,24 +65,24 @@ const displayNews = allNews => {
 
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src="${singleNews.image_url}" class=" img-fluid rounded-start" alt="...">
+                    <img src="${image_url}" class=" img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">${singleNews.title}</h5>
-                    <p class="card-text">${singleNews.details.length > 100 ?singleNews.details.slice(0, 400) + "..." : details}</p>
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text mt-3">${details.length > 300 ?singleNews.details.slice(0, 300) + "..." : details}</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="d-flex align-items-center">
-                            
+                        <img src="${img === 'undifined' ? 'N/A' : img}" class="rounded-circle border" alt="author_name" width="40" height="40">
                             &nbsp; &nbsp;
                             <div>
-                                
-                                <small>${singleNews.author.published_date}</small>
+                                <p class="fw-semibold mb-0">${name ? name : 'N/A'}</p>
+                                <small>${published_date ? published_date : 'N/A'}</small>
                             </div>
                         </div>
-                        <p class="mt-0 mb-0"> <i class="fa-regular fa-eye"></i> ${singleNews.total_view}</p>
+                        <p class="mt-0 mb-0"> <i class="fa-regular fa-eye"></i> ${total_view ? total_view : 'N/A'}</p>
                         <!-- Button trigger modal -->
-                        <i onclick="showDetailsOfNews('${singleNews._id}')" style='font-size:24px' class='far' role="button" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">&#xf35a;</i>
+                        <i onclick="showDetailsOfNews('${_id}')" style='font-size:24px' class='far' role="button" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">&#xf35a;</i>
                     </div>
                 </div>
             </div>
@@ -94,14 +92,14 @@ const displayNews = allNews => {
         `;
         newsContainer.appendChild(newsDiv);
         // spinner stop
-        loadingSpinner.classList.add('d-none');
+        // loadingSpinner.classList.add('d-none');
     });
 }
 
 
 const showDetailsOfNews = newsId => {
     const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displayNewsDatails(data.data[0]))
@@ -114,7 +112,7 @@ const displayNewsDatails = singleNews => {
 
     const breakingNews = document.getElementById('category-01');
     const modalTitle = document.getElementById('newsDetailsModalLabel');
-    modalTitle.innerText = `${breakingNews.innerText}`;
+    modalTitle.innerText = `${title}`;
 
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
@@ -135,4 +133,7 @@ const displayNewsDatails = singleNews => {
             </div>
         </div>
     `;
+    
 }
+
+newsCategoriesContainer()
